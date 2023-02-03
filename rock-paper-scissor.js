defed803962ec5014cffaa74ce3dba000c6467d3
marlucmoon <109116244+Marlucmoon@users.noreply.github.com>
2023-02-03 14:56:43 +0100

    my first commmit

---
+++ rock-paper-scissor.js
@@ -0,0 +1,12 @@
+
+//let options = ["Rock", "Paper", "Scissor"];
+//let choice = options [Math.floor(Math.random()*options.length)];
+
+function getComputerChoice() {
+    let options = ["Rock", "Paper", "Scissor"];
+    let choice = options [Math.floor(Math.random()*options.length)];
+    console.log(choice);
+}
+
+
+getComputerChoice();
