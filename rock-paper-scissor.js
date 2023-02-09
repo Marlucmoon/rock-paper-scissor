65930c8a157d0c578aa23a68100c712660d20517
marlucmoon <109116244+Marlucmoon@users.noreply.github.com>
2023-02-09 02:28:00 +0100

    finish both computer-player choice functions, started architecture and currently playing with winner declaration to dynamically change.

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,12 +1,83 @@
 
-//let options = ["Rock", "Paper", "Scissor"];
-//let choice = options [Math.floor(Math.random()*options.length)];
 
-function getComputerChoice() {
-    let options = ["Rock", "Paper", "Scissor"];
-    let choice = options [Math.floor(Math.random()*options.length)];
-    console.log(choice);
+function getComputerChoice(computerChoice) {
+
+    let options = ['rock', 'paper', 'scissor']
+    computerChoice = options [Math.floor(Math.random()*options.length)];
+    return computerChoice;
+}
+
+let phraseWinner = "You win ! ";
+let phraseLoser = "You lose ! ";
+
+function winner(DeclarationPhrase) {
+    
+    
+    DeclarationPhrase = [phraseWinner + getComputerChoice()];
+    return DeclarationPhrase;
+}
+
+console.log(winner());
+
+
+
+
+/*
+
+function getPlayerChoice(playerChoice) {
+
+    playerChoice = window.prompt("Please choose:") .toLowerCase();
+    return playerChoice;
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(play)
+
+
+
+/*
+
+
+function getComputerChoice(computerChoice) {
+   
+    let options = ['rock', 'paper', 'scissor'];
+    let optionsRandom = options [Math.floor(Math.random()*options.length)];
+    if(optionsRandom == options[0]) {
+        computerChoice = 0;
+        return options[0];
+    }
+    else if(optionsRandom == options[1]) {
+        computerChoice = 1;
+        return options[1];
+    }
+    else {
+        computerChoice = 2;
+        return options[2];
+    }
+    
 }
 
 
-getComputerChoice();
+
+/*
+
+function playRound(playerSelection, computerSelection) {
+return playerSelection;
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
+/*
+Game() {
+
+    for (let i = 0; i < 5; i++) {
+
+    }
+}
+*/
+
